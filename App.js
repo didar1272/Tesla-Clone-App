ffb94d0eae7e53a5598c07e0b0f939e379593bb9
Md. Didarul Islam <reurnzero101@gmail.com>
2021-01-18 01:36:25 +0600

    CarItem Component and folder correction

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import CarItem from './components/CarItem';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      
-      <View style={styles.carContainer}>
-
-        <ImageBackground 
-        source={require('./assets/images/ModelX.jpeg')} 
-        style={styles.image} />
-
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model S</Text>
-          <Text style={styles.subtitle}>Starting at $69,428</Text>
-        </View>
-      </View>
+      <CarItem />
 
       <StatusBar style="auto" />
     </View>
@@ -31,31 +21,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 
-  carContainer: {
-    width: '100%',
-    height: '100%'
-  },
-
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center'
-  },
-
-  title: {
-    fontSize: 40,
-    fontWeight: '500'
-  },
-
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62'
-  },
-
-  image: {
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-    position: 'absolute'
-  }
 });
